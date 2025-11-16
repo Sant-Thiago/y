d42577bb82ef7cfe9618bdb5c77809e4ceac0f61
Sant-Thiago <thiagovieirab2b@gmail.com>
2025-11-16 16:12:28 -0300

    falta responsividade da tela de contato e cardapio

--- src/pages/who_we_are/WhoWeAre.jsx
+++ src/pages/who_we_are/WhoWeAre.jsx
@@ -3,18 +3,35 @@ import Navbar from "../../components/navbar/Navbar";
 import styles from "./WhoWeAre.module.css";
 import img from "@/utils/assets/quem_somos.PNG";
 import companyImg from "@/utils/assets/company.jpeg";
+import firstImage from "@/utils/assets/banner_image.jpeg";
+import secondImage from "@/utils/assets/banner_image_2.jpeg";
 import outSideRestaurant from "@/utils/assets/outter_restaurant.jpg";
 import Footer from "../../components/footer/Footer";
 
+
+
 export default function WhoWeAre({
-    images = [{imagem: companyImg,alt: "imagem 1"}],
-    history = [{
-        class: {fstStyle: styles.cardHistory, sndStyle: styles.hideCard}, 
-        title: "Lorem dolor sit amet consectetur adipisicing elit.",
-        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim repellat fuga, nesciunt saepe quam aspernatur! Dicta, voluptatibus. Quas deserunt expedita iste ut consequatur sit nesciunt ipsum esse. Dolore, saepe!",
-        image: {src: outSideRestaurant, alt: "restaurante 1"},
-        year: "2020"
-    }, ]
+    images = [
+        {src: companyImg, alt: "imagem 1"},
+        {src: firstImage, alt: "first image"},
+        {src: secondImage, alt: "second image"}
+    ],
+    history = [
+        {
+            class: {fstStyle: styles.cardHistory, sndStyle: styles.hideCard}, 
+            title: "Lorem dolor sit amet consectetur adipisicing elit.",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore quos magnam quia at deleniti earum aut maiores porro sed expedita necessitatibus accusamus rerum dolores sit dignissimos, accusantium doloribus assumenda neque? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim repellat fuga, nesciunt saepe quam aspernatur! Dicta, voluptatibus. Quas deserunt expedita iste ut consequatur sit nesciunt ipsum esse. Dolore, saepe!",
+            image: {src: outSideRestaurant, alt: "restaurante 1"},
+            year: "2020"
+        },
+        {
+            class: {fstStyle: styles.cardHistory, sndStyle: styles.hideCard}, 
+            title: "Lorem dolor sit amet consectetur adipisicing elit.",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore quos magnam quia at deleniti earum aut maiores porro sed expedita necessitatibus accusamus rerum dolores sit dignissimos, accusantium doloribus assumenda neque? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati enim repellat fuga, nesciunt saepe quam aspernatur! Dicta, voluptatibus. Quas deserunt expedita iste ut consequatur sit nesciunt ipsum esse. Dolore, saepe!",
+            image: {src: outSideRestaurant, alt: "restaurante 1"},
+            year: "2020"
+        }
+    ]
 }) {
     return(
         <>
@@ -24,24 +41,36 @@ export default function WhoWeAre({
                     image={img}
                     text={"Quem Somos"}
                 />
-                <section className={styles.wrapperText}>
-                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ducimus architecto nobis, est earum expedita magni consequatur iure ratione voluptatibus laborum repudiandae cupiditate quam ex fugiat ab nam non? Iste.</p>
+                <section className={styles.background}>
+                    <div className={styles.wrapper}>
+                        <div className={styles.wrapperText}>
+                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ducimus architecto nobis, est earum expedita magni consequatur iure ratione voluptatibus laborum repudiandae cupiditate quam ex fugiat ab nam non? Iste.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore corporis explicabo ea quis neque quibusdam voluptates ex facere a qui eveniet, optio possimus consequuntur incidunt. Earum obcaecati illo aspernatur aliquam!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ducimus architecto nobis, est earum expedita magni consequatur iure ratione voluptatibus laborum repudiandae cupiditate quam ex fugiat ab nam non? Iste.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deserunt assumenda autem eum dolorum hic, eveniet laudantium sit perspiciatis natus itaque repellat placeat nemo quasi error sapiente fugit ad amet. orem Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error quam eligendi eius tempore quisquam adipisci tenetur, obcaecati ea recusandae inventore cum ullam minima natus nam itaque ut voluptatibus excepturi accusantium! Lorem</p>
+                        </div>
+                        <div className={styles.wrapperImage}>
+                            <h2>Visite-nos e encante-se!</h2>
+                            <div className={styles.images}>
+                                {images && images.map(it => (
+                                    <img src={it.src} alt={it.alt} />
+                                ))}
+                            </div>
+                        </div>
 
-                </section>
-                <section className={styles.wrapperImage}>
-                    <h2>Visite-nos e encante-se!</h2>
-                    <div className={styles.images}>
-                        {images && images.map(it => (
-                            <img src={it.imagem} alt={it.alt} />
-                        ))}
                     </div>
                 </section>
                 <section className={styles.history}>
                         <h2>Conheça nossa linha do tempo de sucesso!</h2>
-                        <div className={styles.divisor}>
+                            <div className={styles.divisor}>
                             {history && history.map((it, idx) => (
-                                <div className={styles.wrapperCardHistory} key={idx}>
+                                <div 
+                                    key={idx}
+                                    className={`
+                                        ${styles.wrapperCardHistory}
+                                        ${(idx % 2 == 0) ? styles.reverse : ""}
+                                    `}  // Nesse codigo posso tanto usar essa condição para revesar os lados das imagens quanto alternar o class.fstStyle e sndStyle
+                                >
                                     <div className={it.class.fstStyle}>
                                         <h3>{it.title}</h3>
                                         <p>{it.text}</p>
